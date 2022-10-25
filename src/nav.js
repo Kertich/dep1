696d0dd4e737cacc9660505651099d533676f329
Kertich <kipsangkertich@gmail.com>
2022-10-25 23:38:42 +0300

    nav bar

---
+++ src/nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './nav.css'
+
+function nav() {
+  return (
+    <div className='nav'>
+        <div className='btn'>
+        <button>Home</button>
+        <button>About</button>
+        <button>Contact me</button>
+      </div>
+      
+      
+      </div>
+  )
+}
+
+export default nav
